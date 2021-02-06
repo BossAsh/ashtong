a5022a286e9212ef3d90a541c950b32343dca06f
BossAsh <61855575+BossAsh@users.noreply.github.com>
2021-02-06 17:30:30 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+myFunction(){
+	var txt;
+  	var token = prompt("Please enter your token for placing Orders!");
+  if (token ==  "bakaashtongyan") {
+    window.open(place.html);
+  } else {
+    txt = "Wrong Token";
+  }
+}
